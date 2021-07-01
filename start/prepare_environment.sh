be90d3044cbdcbf6e0ee5b27f0dad3fa8b3a1229
DamanWalia <82758001+DamanWalia@users.noreply.github.com>
2021-07-01 08:20:06 +0200

    Update prepare_environment.sh

--- start/prepare_environment.sh
+++ start/prepare_environment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Creating ecomm-account Service Account"
+gcloud iam service-accounts create ecomm-account --display-name "Ecomm Account"
+gcloud iam service-accounts keys create key.json --iam-account=ecomm-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
+
+echo "Setting ecomm-account IAM Role"
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAccount:ecomm-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role roles/owner
+
 echo "Creating Datastore/App Engine instance"
 gcloud app create --region "us-central"
 
@@ -22,20 +29,15 @@ pip install -r requirements.txt
 echo "Creating Datastore entities"
 python add_entities.py
 
-echo "Creating ecomm-account Service Account"
-gcloud iam service-accounts create ecomm-account --display-name "Ecomm Account"
-gcloud iam service-accounts keys create key.json --iam-account=ecomm-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
+echo "Export credentials key.json"
 export GOOGLE_APPLICATION_CREDENTIALS=key.json
 
-echo "Setting ecomm-account IAM Role"
-gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member serviceAccount:ecomm-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role roles/owner
-
 echo "Creating Cloud Pub/Sub topic"
-gcloud beta pubsub topics create feedback
-gcloud beta pubsub subscriptions create worker-subscription --topic feedback
+gcloud pubsub topics create feedback
+gcloud pubsub subscriptions create worker-subscription --topic feedback
 
 echo "Creating Cloud Spanner Instance, Database, and Table"
 gcloud spanner instances create ecomm-instance --config=regional-us-central1 --description="Ecomm instance" --nodes=1
-gcloud spanner databases create ecomm-database --instance ecomm-instance --ddl "CREATE TABLE Orders ( feedbackId STRING(100) NOT NULL, email STRING(100), ecommSTRING(20), feedback STRING(MAX), rating INT64, score FLOAT64, timestamp INT64 ) PRIMARY KEY (feedbackId);"
+gcloud spanner databases create ecomm-database --instance ecomm-instance --ddl "CREATE TABLE Feedback ( feedbackId STRING(100) NOT NULL, email STRING(100), ecomm STRING(20), feedback STRING(MAX), rating INT64, score FLOAT64, timestamp INT64 ) PRIMARY KEY (feedbackId);"
 
 echo "Project ID: $DEVSHELL_PROJECT_ID"
